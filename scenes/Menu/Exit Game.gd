26018d4596cc2d07a6d28633d22b9479c0f33688
Tim Yuen <timothy.yuen22@gmail.com>
2018-08-12 16:39:05 -0400

    Final commit for LD42

--- scenes/Menu/Exit Game.gd
+++ scenes/Menu/Exit Game.gd
@@ -1,7 +1,15 @@
 extends Button
 
+var select_sound
+
 func _ready():
+	select_sound = $SelectSound
+	
+	self.connect("pressed", self, "_play_select_sound")
 	self.connect("pressed", self, "_exit_game")
 
 func _exit_game():
 	get_tree().quit()
+
+func _play_select_sound():
+	select_sound.play()
--- scenes/Menu/Singleplayer.gd
+++ scenes/Menu/Singleplayer.gd
@@ -1,4 +1,12 @@
 extends Button
 
+var select_sound
+
 func _ready():
+	select_sound = $SelectSound
+	
+	self.connect("pressed", self, "_play_select_sound")
 	self.connect("pressed", Global, "change_state", [Global.SINGLEPLAYER])
+
+func _play_select_sound():
+	select_sound.play()
